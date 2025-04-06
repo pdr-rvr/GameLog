4fc15465d73168536dc37b46584c6da0524daf4e
Siq-Andre <siq.andre2302@gmail.com>
2025-04-06 14:53:34 -0300

    Added configuration for databse connection

--- GameLog_Backend/Program.cs
+++ GameLog_Backend/Program.cs
@@ -1,8 +1,32 @@
+using DotNetEnv;
+using GameLog_Backend.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+Env.Load();
+
+
+var baseConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var completeConnectionString = baseConnectionString
+    .Replace("{DB_SERVER}", Environment.GetEnvironmentVariable("DB_SERVER"))
+    .Replace("{DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+    .Replace("{DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+    .Replace("{DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
+
+builder.Services.AddDbContext<GameLogContext>(options =>
+    options.UseSqlServer(completeConnectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
