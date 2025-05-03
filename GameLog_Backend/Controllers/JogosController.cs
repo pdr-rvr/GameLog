d38232c2383360c67d2306d7b442ae55c1a81f08
Siq-Andre <siq.andre2302@gmail.com>
2025-05-03 09:38:06 -0300

    Created controller for Jogos

---
+++ GameLog_Backend/Controllers/JogosController.cs
@@ -0,0 +1,33 @@
+﻿using GameLog_Backend.DTOs;
+using GameLog_Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GameLog_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JogosController : ControllerBase
+    {
+        private readonly JogoServices _jogoServices;
+
+        public JogosController(JogoServices jogoServices)
+        {
+            _jogoServices = jogoServices;
+        }
+
+        [HttpGet]
+        public IActionResult ListarTodosJogos()
+        {
+            try
+            {
+                var jogos = _jogoServices.ListarJogos();
+                return Ok(jogos);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Internal server error: " + ex.Message });
+            }
+        }
+    }
+}
