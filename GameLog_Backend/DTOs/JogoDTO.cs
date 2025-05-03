3699259972ddcc2fbf0945e3a5284657b63691bf
Siq-Andre <siq.andre2302@gmail.com>
2025-05-03 09:37:24 -0300

    Created DTO for Jogo

---
+++ GameLog_Backend/DTOs/JogoDTO.cs
@@ -0,0 +1,16 @@
+﻿using GameLog_Backend.Entities;
+
+namespace GameLog_Backend.DTOs
+{
+    public class JogoDTO
+    {
+        public int JogoId {  get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public DateOnly DataLancamento { get; set; }
+        public int ClassificacaoIndicativa { get; set; }
+        public int EmpresaId { get; set; }
+        public bool EstaAtivo { get; set; }
+    }
+}
