acd9f713ef06f67f851e2260e3606e1e366d5806
Siq-Andre <siq.andre2302@gmail.com>
2025-05-10 23:07:10 -0300

    Created dto and mapper for usuarios

---
+++ GameLog_Backend/DTOs/UsuarioDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace GameLog_Backend.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int UsuarioId { get; set; }
+        public string NomeUsuario { get; set; }
+        public string Email { get; set; }
+        public string FotoDePerfil { get; set; }
+        public bool EstaAtivo { get; set; }
+    }
+
+    public class CriarUsuarioDTO
+    {
+        public string NomeUsuario { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string FotoDePerfil { get; set; }
+    }
+
+    public class UsuarioLoginDTO
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ GameLog_Backend/Profiles/UsuarioProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GameLog_Backend.DTOs;
+using GameLog_Backend.Entities;
+
+namespace GameLog_Backend.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<Usuario, UsuarioDTO>();
+            CreateMap<CriarUsuarioDTO, Usuario>()
+                .ForMember(dest => dest.Senha, opt => opt.Ignore()); 
+        }
+    }
+}
