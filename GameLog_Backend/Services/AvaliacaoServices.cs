da6a46e312188875481acb0ebe94cd248ce6b1c2
Siq-Andre <siq.andre2302@gmail.com>
2025-05-18 20:04:00 -0300

    Created services for avaliacao

---
+++ GameLog_Backend/Services/AvaliacaoServices.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using GameLog_Backend.Database;
+using GameLog_Backend.DTOs;
+using GameLog_Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameLog_Backend.Services
+{
+    public class AvaliacaoServices
+    {
+        private readonly GameLogContext _context;
+        private readonly IMapper _mapper;
+
+        public AvaliacaoServices(GameLogContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        private async Task<Jogo> VerificarJogoExiste(int jogoId)
+        {
+            var jogo = await _context.Jogos.FindAsync(jogoId);
+            if (jogo == null)
+                throw new Exception("Jogo não encontrado");
+            return jogo;
+        }
+
+        private async Task VerificarAvaliacaoDuplicada(int usuarioId, int jogoId)
+        {
+            var usuarioComAvaliacoes = await _context.Usuarios
+                .Include(u => u.Avaliacoes)
+                .ThenInclude(a => a.Jogo)
+                .FirstOrDefaultAsync(u => u.Id == usuarioId);
+
+            if (usuarioComAvaliacoes == null)
+                throw new Exception("Usuário não encontrado");
+
+            if (usuarioComAvaliacoes.Avaliacoes.Any(a =>
+                a.Jogo != null && a.Jogo.Id == jogoId && a.EstaAtivo))
+            {
+                throw new Exception("Você já possui uma avaliação ativa para este jogo");
+            }
+        }
+
+        public async Task<AvaliacaoDTO> CriarAvaliacao(CriarAvaliacaoDTO avaliacaoDTO, int usuarioId)
+        {
+            await VerificarJogoExiste(avaliacaoDTO.JogoId);
+            await VerificarAvaliacaoDuplicada(usuarioId, avaliacaoDTO.JogoId);
+
+            var avaliacao = _mapper.Map<Avaliacao>(avaliacaoDTO);
+            avaliacao.DataPublicacao = DateTime.UtcNow;
+            avaliacao.EstaAtivo = true;
+            avaliacao.Jogo = await _context.Jogos.FindAsync(avaliacaoDTO.JogoId);
+
+            var usuario = await _context.Usuarios.FindAsync(usuarioId);
+            usuario?.Avaliacoes.Add(avaliacao);
+
+            await _context.SaveChangesAsync();
+            return _mapper.Map<AvaliacaoDTO>(avaliacao);
+        }
+
+        public async Task<IEnumerable<AvaliacaoDTO>> ListarAvaliacoes()
+        {
+            var avaliacoes = await _context.Avaliacoes
+                .Where(a => a.EstaAtivo)
+                .OrderByDescending(a => a.DataPublicacao)
+                .Include(a => a.CurtidasDeAvaliacao)
+                .ToListAsync();
+
+            return _mapper.Map<IEnumerable<AvaliacaoDTO>>(avaliacoes);
+        }
+
+        public async Task<AvaliacaoDTO?> ObterAvaliacaoPorId(int id)
+        {
+            var avaliacao = await _context.Avaliacoes
+                .Include(a => a.CurtidasDeAvaliacao)
+                .FirstOrDefaultAsync(a => a.Id == id && a.EstaAtivo);
+
+            return avaliacao != null ? _mapper.Map<AvaliacaoDTO>(avaliacao) : null;
+        }
+
+        public async Task<IEnumerable<AvaliacaoDTO>> ListarAvaliacoesPorUsuario(int usuarioId)
+        {
+            var avaliacoes = await _context.Usuarios
+                .Where(u => u.Id == usuarioId)
+                .SelectMany(u => u.Avaliacoes)
+                .Where(a => a.EstaAtivo)
+                .OrderByDescending(a => a.DataPublicacao)
+                .Include(a => a.Jogo)  
+                .Include(a => a.CurtidasDeAvaliacao)
+                .Select(a => new AvaliacaoDTO
+                {
+                    AvaliacaoId = a.Id,
+                    Nota = a.Nota,
+                    JogoId = a.Jogo.Id,
+                    TextoAvaliacao = a.TextoAvaliacao,
+                    DataPublicacao = a.DataPublicacao,
+                    TotalCurtidas = a.CurtidasDeAvaliacao.Count
+                })
+                .ToListAsync();
+
+            return avaliacoes;
+        }
+
+        public async Task<AvaliacaoDTO?> EditarAvaliacao(int id, EditarAvaliacaoDTO avaliacaoDTO, int usuarioId)
+        {
+            var avaliacao = await _context.Usuarios
+                .Where(u => u.Id == usuarioId)
+                .SelectMany(u => u.Avaliacoes)
+                .FirstOrDefaultAsync(a => a.Id == id && a.EstaAtivo);
+
+            if (avaliacao == null)
+                return null;
+
+            _mapper.Map(avaliacaoDTO, avaliacao);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<AvaliacaoDTO>(avaliacao);
+        }
+
+        public async Task<bool> DeletarAvaliacao(int id, int usuarioId)
+        {
+            var avaliacao = await _context.Usuarios
+                .Where(u => u.Id == usuarioId)
+                .SelectMany(u => u.Avaliacoes)
+                .FirstOrDefaultAsync(a => a.Id == id && a.EstaAtivo);
+
+            if (avaliacao == null)
+                return false;
+
+            avaliacao.EstaAtivo = false; 
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
