e4279b38c8c084891cb96bd334e7aeecf185cd07
Siq-Andre <siq.andre2302@gmail.com>
2025-05-17 22:04:26 -0300

    Added map for edit user

--- GameLog_Backend/Profiles/UsuarioProfile.cs
+++ GameLog_Backend/Profiles/UsuarioProfile.cs
@@ -9,8 +9,15 @@ namespace GameLog_Backend.Profiles
         public UsuarioProfile()
         {
             CreateMap<Usuario, UsuarioDTO>();
+
             CreateMap<CriarUsuarioDTO, Usuario>()
-                .ForMember(dest => dest.Senha, opt => opt.Ignore()); 
+                .ForMember(dest => dest.Senha, opt => opt.Ignore());
+
+            CreateMap<EditarUsuarioDTO, Usuario>()
+                .ForMember(dest => dest.Senha, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForAllMembers(opts =>
+                    opts.Condition((src, dest, srcMember) => srcMember != null));
         }
     }
 }
