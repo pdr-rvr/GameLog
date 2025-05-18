b0cb0c5401cbd31a3ff781bc528bb07aa4f3bf81
Siq-Andre <siq.andre2302@gmail.com>
2025-05-18 20:03:37 -0300

    Added Constraint to Notas in Avaliacao to be between 0 and 5

---
+++ GameLog_Backend/Migrations/20250518225928_EscopodeNotasDeAvaliacao.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameLog_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class EscopodeNotasDeAvaliacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Avaliacao_Nota_Range",
+                table: "Avaliacoes",
+                sql: "[Nota] >= 0 AND [Nota] <= 5");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Avaliacao_Nota_Range",
+                table: "Avaliacoes");
+        }
+    }
+}
